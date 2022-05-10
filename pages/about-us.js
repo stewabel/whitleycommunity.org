064b0e2a448456d657f7b241077c84b666aef5a9
Stew Abel <stew@grupettosoftware.com>
2022-05-10 18:24:30 +0100

    added history page

--- pages/about-us.js
+++ pages/about-us.js
@@ -16,8 +16,8 @@ export default function CentreHire() {
   return (
     <>
       <Head>
-        <title>WCA - Hiring our Centre</title>
-        <meta name="description" content="WCA - Hiring our Centre" />
+        <title>WCA - About Us</title>
+        <meta name="description" content="WCA - About Us" />
       </Head>
       <View
         as="div"
@@ -48,11 +48,13 @@ export default function CentreHire() {
             gap={tokens.space.xs}
           >
             <Heading level={5}>
-              Hiring Our Centre
+              About Us
             </Heading>
 
             <Text as="span">
-              
+            <p>Whitley Community Centre is a beautiful former school in the village of Whitley Lower, just between Mirfield and Dewsbury.</p>
+            <p>The Centre is run by a group of dedicated volunteers and is a registered charity. All funds raised go back to preserving the building for future generations and supporting our local community.</p>
+            <p>The Centre is available for hire and runs events for the community on a regular basis.</p>
             </Text>
           </Flex>
         </Flex>
