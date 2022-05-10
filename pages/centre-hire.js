e9e7d0d22ba8dd6719d77d27c9e6c03d2f2b1589
Stew Abel <stew@grupettosoftware.com>
2022-05-10 18:02:15 +0100

    Updates to title and head of hire page

--- pages/centre-hire.js
+++ pages/centre-hire.js
@@ -1,20 +1,41 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import { View, Card } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function CentreHire() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Whitley Community Association</title>
-        <meta name="description" content="Whitley Community Association" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>WCA - Hiring our Centre</title>
+        <meta name="description" content="WCA - Hiring our Centre" />
       </Head>
-
-      <main className={styles.main}>
+      <View
+        as="div"
+        ariaLabel="View example"
+        backgroundColor="var(--amplify-colors-white)"
+        padding="1rem"
+        width="100%"
+        >
         <h1 className={styles.title}>
-          Whitley Community Centre
+          WCA - Hiring our Community Centre
         </h1>
-      </main>
-    </div>
+      </View>
+      <Card
+        ariaLabel="View example"
+        backgroundColor="var(--amplify-colors-white)"
+        padding="1rem"
+        style={{  position: 'relative', width: '100vw'}}
+        >
+        <Image src="/centre-aerial-1.jpg"
+                alt="Whitley Community Centre. An 1800s, Yorkshire stone built former school."
+                layout="responsive"
+                width="828"
+                height="465"
+                objectFit="fit"
+        />
+      </Card>
+    </>
   )
 }
