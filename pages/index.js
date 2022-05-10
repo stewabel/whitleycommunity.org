158374dd735c1ed05686da4529e38cdd06b6a1f8
Stew Abel <stew@grupettosoftware.com>
2022-05-10 08:44:11 +0100

    added image to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
-import Link from 'next/link'
+import Image from 'next/image'
+import { View, Card } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -12,9 +14,33 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+      <View
+        as="div"
+        ariaLabel="View example"
+        backgroundColor="var(--amplify-colors-white)"
+        padding="1rem"
+        width="100%"
+        >
         <h1 className={styles.title}>
           Whitley Community Association
         </h1>
+      </View>
+
+      <Card
+        ariaLabel="View example"
+        backgroundColor="var(--amplify-colors-white)"
+        padding="1rem"
+        style={{  position: 'relative', width: '100vw'}}
+        >
+        <Image src="/centre-aerial-1.jpg"
+               alt="Whitley Community Centre. An 1800s, Yorkshire stone built former school."
+               layout="responsive"
+               width="828"
+               height="465"
+               objectFit="fit"
+        />
+        
+        </Card>
       </main>
     </div>
   )
